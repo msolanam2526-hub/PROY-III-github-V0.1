d25c710a91019f4a1fcdbee2f45c279f200e3c0b
Ursel23 <hurselv2526@politecnics.barcelona>
2026-03-25 13:26:13 +0100

    commit v0.2

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,19 @@ public class Main {
         System.out.println("Bienvenido al Jugador:");
         opcion = sc.nextInt();
         switch (opcion){
-            case 1:
+            case 1: {
+                System.out.println("1.- ");
                 break;
-            case 2:
+            } case 2: {
+                System.out.println("2.- ");
                 break;
-            case 3:
+            } case 3: {
+                System.out.println("3.- ");
                 break;
-            case 4:
+            } case 4: {
+                System.out.println("4. ");
                 break;
+            }
         }
     }
 }
