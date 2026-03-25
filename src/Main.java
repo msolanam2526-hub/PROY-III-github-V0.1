a0d7c33b1ab5e91bd673cfb09d9f6077f6daa334
msolanam2526-hub <msolanam2526@politecnics.barcelona>
2026-03-25 13:06:00 +0100

    Comit V0.1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuJugadores();
+    }
+    public static void MenuJugadores(){
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido al Jugador:");
+        opcion = sc.nextInt();
+        switch (opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
